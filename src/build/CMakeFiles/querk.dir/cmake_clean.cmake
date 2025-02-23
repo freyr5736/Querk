322cdb2cdea504f1f6c80c2fe6dc5eced503c5d6
freyr5736 <bhaskark.kuniyal@gmail.com>
2025-02-23 15:24:54 +0000

    build files

---
+++ src/build/CMakeFiles/querk.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/querk.dir/main.cpp.o"
+  "CMakeFiles/querk.dir/main.cpp.o.d"
+  "querk"
+  "querk.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/querk.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/build/CMakeFiles/querk.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for querk.
+# This may be replaced when dependencies are built.
