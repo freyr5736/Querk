11b44035a74b8b0ebae44c5ba4f35cd5ce4ec64a
freyr5736 <bhaskark.kuniyal@gmail.com>
2025-09-15 07:41:56 +0000

    commments

--- src/build/out.asm
+++ src/build/out.asm
@@ -4,22 +4,6 @@ _start:
     push rax
     mov rax, 0
     push rax
-    push QWORD [rsp + 0]
-    pop rax
-    test rax, rax
-    jz label0
-    mov rax, 20
-    push rax
-    mov rax, 60
-    pop rdi
-    syscall
-    add rsp, 0
-label0:
-    mov rax, 10
-    push rax
-    mov rax, 60
-    pop rdi
-    syscall
     mov rax, 60
     mov rdi, 0
     syscall
